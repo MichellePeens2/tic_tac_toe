7cb6497fb90678cbf3bdf6b19257fdc09ce52b46
Michelle Peens <michelle.peens@shopify.com>
2021-10-29 13:31:44 -0400

    QuickCommit

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,65 +1,80 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Form, TextField, Button, Header, Board, Error } from "./components";
 
-function TextField({label, value, setValue}: {label: string, value: string, setValue: (value: string) => void}) {
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value)
-  }
-
-  return (
-    <>
-      <label>{label}: </label>
-      <input type="text" name="firstName" value={value} onChange={onChange}/>
-      <br/><br/>
-    </>
-  )
-}
+// How to add SCSS
+import "./App.css";
 
 function App() {
-  const [firstName, setFirstName] = useState(''); // takes the initial value of the state variable as an argument
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
+  const [playerOne, setPlayerOne] = useState("");
+  const [playerTwo, setPlayerTwo] = useState("");
+  const [displayBoard, setDisplayBoard] = useState(false);
+  const [errors, setErrors] = useState<string[]>([]);
+  const [didSubmit, setDidSubmit] = useState(false);
 
-  // const [firstName, setFirstName] = useState(() => setDefaultFirstName()); 
-  
-  // const [prevFirstName, setPrevFirstName] = useState('');
-  // const [prevLastName, setPrevLastName] = useState('');
-  // const [prevEmail, setPrevEmail] = useState('');
+  const play = () => {
+    setErrors([]);
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault(); // no refresh
-    console.log('test');
-  }
+    if (playerOne === "") setErrors(["Player one is blank."]);
+    if (playerTwo === "")
+      setErrors((prev) => [...prev, "Player two is blank."]);
+
+    setDidSubmit(true);
+  };
+
+  // useEffect(() => {
+  //   document.addEvenetListener("onScroll", doSomething);
+  //   console.log("hello");
+  //   return () => {
+  //     document.removeEventListener("onScroll", doSomething);
+  //   };
+  // }, []);
+
+  useEffect(() => {
+    setDisplayBoard(didSubmit && errors.length === 0);
+  }, [errors, didSubmit]);
+
+  // mount
+  // add
+  // // state change ->
+  // // remove & add
+  // // state change
+  // // remove & add
+  // unmount
+  // remove
+
+  const restart = () => {
+    setPlayerOne("");
+    setPlayerTwo("");
+    setErrors([]);
+    setDisplayBoard(false);
+    setDidSubmit(false);
+  };
 
   return (
     <div className="App">
-      <form className="form" onSubmit={handleSubmit}>  
-        <TextField
-          label='firstName'
-          value={firstName}
-          setValue={setFirstName}
-        />
-        <TextField
-          label='lastName'
-          value={lastName}
-          setValue={setLastName}
-        />
-        <TextField
-          label='email'
-          value={email}
-          setValue={setEmail}
-        />
-        <input type="submit" value="Submit" />      
-      </form>
+      <Header title="Tic Tac Toe" />
 
-      <br/>
-      <br/>
+      {/* Warning: Each child in a list should have a unique "key" prop. */}
+      {errors.map((error, index) => {
+        return <Error key={index} message={error} />;
+      })}
 
-      <div className="result">
-        <p> First name: {firstName}  </p>
-        <p> Last name: {lastName} </p>
-        <p> Email: {email}</p>
-      </div>
+      {displayBoard ? (
+        <>
+          <Board />
+          <Button type="secondary" onClick={restart}>
+            Restart
+          </Button>
+        </>
+      ) : (
+        <Form>
+          <TextField label="X" value={playerOne} setValue={setPlayerOne} />
+          <TextField label="O" value={playerTwo} setValue={setPlayerTwo} />
+          <Button type="primary" onClick={play}>
+            Play
+          </Button>
+        </Form>
+      )}
     </div>
   );
 }
