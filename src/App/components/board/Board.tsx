f8abc4dfd80da74d16fbbcfeff99edc3d1dd2df7
Michelle Peens <michelle.peens@shopify.com>
2021-11-02 20:13:48 -0400

    QuickCommit

--- src/App/components/board/Board.tsx
+++ src/App/components/board/Board.tsx
@@ -5,64 +5,112 @@ import "./Board.css";
 
 const CELLS = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-export default function Board({ playerOne, playerTwo, onReset }: { playerOne: Player, playerTwo: Player, onReset: () => void }) {
-  const [board, setBoard] = useState<Symbol[]>(CELLS.map(() => ''));
+function Block({
+  board,
+  index,
+  onClick,
+}: {
+  board: Symbol[];
+  index: number;
+  onClick: (index: number) => void;
+}) {
+  return (
+    <div
+      id={`block${index}`}
+      className={
+        `Block 
+        ${board[index] === "X" ? "XClass" : ""} 
+        ${board[index] === "O" ? "OClass" : ""}`
+      }
+      onClick={() => onClick(index)}
+    />
+  );
+}
+
+export default function Board({
+  playerOne,
+  playerTwo,
+  onReset,
+}: {
+  playerOne: Player;
+  playerTwo: Player;
+  onReset: () => void;
+}) {
+  const [board, setBoard] = useState<Symbol[]>(CELLS.map(() => ""));
   const [currentPlayer, setCurrentPlayer] = useState<Player>(playerOne);
   const [winner, setWinner] = useState<Player | null>(null);
 
   const onClick = (index: number) => {
-    setBoard(prevBoard => {
-        const newBoardState = [...prevBoard];    
-        newBoardState[index] = currentPlayer.symbol; 
-        currentPlayer.symbol === 'X' ? 
-          setCurrentPlayer(playerTwo) : 
-          setCurrentPlayer(playerOne)
-        return newBoardState;
-    })
-  }
+    setBoard((prevBoard) => {
+      const newBoardState = [...prevBoard];
+      newBoardState[index] = currentPlayer.symbol;
+      currentPlayer.symbol === "X"
+        ? setCurrentPlayer(playerTwo)
+        : setCurrentPlayer(playerOne);
+      return newBoardState;
+    });
+  };
 
-  const eveluateCells = (board: Symbol[], c1: number, c2: number, c3: number): Symbol | null => {
-    if (board[c1] !== '' && board[c1] === board[c2] && board[c2] === board[c3]) 
-      return board[c1] 
+  const eveluateCells = (
+    board: Symbol[],
+    c1: number,
+    c2: number,
+    c3: number
+  ): Symbol | null => {
+    if (board[c1] !== "" && board[c1] === board[c2] && board[c2] === board[c3])
+      return board[c1];
+    return null;
+  };
+
+  const evaluateRows = (board: Symbol[]): Symbol | null => { 
+    [0, 3, 6].forEach((value) => {
+      let ws = eveluateCells(board, value, value+1, value+2);  
+      if(ws) return ws;
+    });
     return null;
   }
 
-  const evaluate = useCallback((board: Symbol[]): void => {
-    let winning_symbol = eveluateCells(board, 0, 1, 2);
+  const evaluate = useCallback(
+    (board: Symbol[]): void => {
+      let ws = eveluateCells(board, 0, 1, 2);
+      if (!ws) ws = eveluateCells(board, 3, 4, 5);
+      if (!ws) ws = eveluateCells(board, 6, 7, 8);
 
-    if(playerOne.symbol === winning_symbol) setWinner(playerOne);
-    if(playerTwo.symbol === winning_symbol) setWinner(playerTwo);
+      if (!ws) ws = eveluateCells(board, 0, 3, 6);
+      if (!ws) ws = eveluateCells(board, 1, 4, 7);
+      if (!ws) ws = eveluateCells(board, 2, 5, 8);
 
-  },[playerOne, playerTwo])
+      if (!ws) ws = eveluateCells(board, 0, 4, 8);
+      if (!ws) ws = eveluateCells(board, 2, 4, 6);
+
+      if (playerOne.symbol === ws) setWinner(playerOne);
+      if (playerTwo.symbol === ws) setWinner(playerTwo);
+    },
+    [playerOne, playerTwo]
+  );
 
   useEffect(() => {
     evaluate(board);
-  }, [board, evaluate])
-    
+  }, [board, evaluate]);
+
   return (
     <>
       <div className="Player">
         <p>{playerOne.name}</p>
         <p>{playerOne.symbol}</p>
-        <p className="Winner">{winner?.name === playerOne.name && 'WINNER'}</p>
+        <p className="Winner">{winner?.name === playerOne.name && "WINNER"}</p>
       </div>
 
       <div className="Player">
         <p>{playerTwo.name}</p>
         <p>{playerTwo.symbol}</p>
-        <p className="Winner">{winner?.name === playerTwo.name && 'WINNER'}</p>
+        <p className="Winner">{winner?.name === playerTwo.name && "WINNER"}</p>
       </div>
 
       <div className="Board">
-        <div id="block0" className={`Block ${board[0] === 'X' && 'XClass'} ${board[0] === 'O' && 'OClass'}`} onClick={() => onClick(0)}></div>
-        <div id="block1" className={`Block ${board[1] === 'X' && 'XClass'} ${board[1] === 'O' && 'OClass'}`} onClick={() => onClick(1)}></div>
-        <div id="block2" className={`Block ${board[2] === 'X' && 'XClass'} ${board[2] === 'O' && 'OClass'}`} onClick={() => onClick(2)}></div>
-        <div id="block3" className={`Block ${board[3] === 'X' && 'XClass'} ${board[3] === 'O' && 'OClass'}`} onClick={() => onClick(3)}></div>
-        <div id="block4" className={`Block ${board[4] === 'X' && 'XClass'} ${board[4] === 'O' && 'OClass'}`} onClick={() => onClick(4)}></div>
-        <div id="block5" className={`Block ${board[5] === 'X' && 'XClass'} ${board[5] === 'O' && 'OClass'}`} onClick={() => onClick(5)}></div>
-        <div id="block6" className={`Block ${board[6] === 'X' && 'XClass'} ${board[6] === 'O' && 'OClass'}`} onClick={() => onClick(6)}></div>
-        <div id="block7" className={`Block ${board[7] === 'X' && 'XClass'} ${board[7] === 'O' && 'OClass'}`} onClick={() => onClick(7)}></div>
-        <div id="block8" className={`Block ${board[8] === 'X' && 'XClass'} ${board[8] === 'O' && 'OClass'}`} onClick={() => onClick(8)}></div>
+        {CELLS.map((_, index) => {
+          return <Block index={index} onClick={onClick} board={board} />;
+        })}
       </div>
 
       <Button type="primary" onClick={onReset}>
